72993ea0556e55a98034f01e20ef2c90c443867c
rgardler-msft <ross.gardler@microsoft.com>
2026-02-15 02:00:20 -0800

    SA-0MLE81V501P6IB77: Place ampa plugin file into skill resources and remove wl_ampa stub/old plugin dir

--- skill/install-ampa/resources/wl_ampa/ampa.mjs
+++
@@ -1,4 +0,0 @@
-// Bundled copy of the plugin moved from plugins/wl_ampa/ampa.mjs
-// Keep original source here for the skill's resource bundle.
-
-export { default } from "../../../plugins/wl_ampa/ampa.mjs";
